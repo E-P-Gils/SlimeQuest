36785a7fe61c2d75a6e0ed54205fe51b0495d22b
unknown <sculptoid11@gmail.com>
2025-01-15 21:12:27 -0500

    Map Elements Added

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const WALL_SLIDE_ACCEL = -10
-const CLIMB_SPEED = 100
 const JUMP_VELOCITY = -50.0
 const GRAVITY = 200  # Gravity value for your game
 
@@ -20,8 +18,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func walk_state(delta):
-	# Transition to wall state if the character is only touching a wall
-	if is_on_wall_only():
+	# Only allow transition to wall state when near a wall and pressing "ui_up"
+	if is_on_wall() and Input.is_action_just_pressed("ui_up"):
 		state = STATES.WALL
 		wall_state(delta)
 		return
@@ -32,6 +30,7 @@ func walk_state(delta):
 	# Jump if the "ui_up" action is pressed and character is on the floor
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("WallClimb")  # Play wallclimb animation when jumping
 
 	# Handle horizontal movement based on left/right inputs
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -40,6 +39,10 @@ func walk_state(delta):
 	else:
 		# Slow down and stop if no horizontal input
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Return to normal state when on the floor (Idle or walking)
+	if is_on_floor():
+		animated_sprite.play("Idle")  # Play idle animation when on the floor
 
 func wall_state(delta):
 	# Transition back to walk state if not on a wall
@@ -54,16 +57,19 @@ func wall_state(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = JUMP_VELOCITY
 		state = STATES.WALK  # Return to walk state after jump
-		animated_sprite.play("Idle")  # Set to idle animation when jumping off the wall
+		animated_sprite.play("WallClimb")  # Play wallclimb animation when jumping off the wall
 		return
 
-	# Handle climbing when moving up/down (use 'ui_down' to control climbing)
-	var direction = Input.get_axis("ui_down", "")
-	if direction != 0:
-		velocity.y = direction * CLIMB_SPEED
-		animated_sprite.play("WallClimb")  # Play wall climbing animation
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		animated_sprite.play("Idle")  # Return to idle animation when climbing stops
-
-	
+	# Handle left/right boost jumps from wall
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x = -SPEED * 1.5  # Launch left with a boost
+		velocity.y = JUMP_VELOCITY
+		state = STATES.WALK
+		animated_sprite.play("WallClimb")  # Play wallclimb animation when jumping off the wall
+		return
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x = SPEED * 1.5  # Launch right with a boost
+		velocity.y = JUMP_VELOCITY
+		state = STATES.WALK
+		animated_sprite.play("WallClimb")  # Play wallclimb animation when jumping off the wall
+		return
